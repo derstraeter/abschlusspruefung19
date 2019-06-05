928dfcd6477820e698917111baaf9899874e93db
derstraeter <mstraeter@gmail.com>
2019-06-05 02:55:32 +0200

    Document css and js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+// GLIDE.JS initalisieren
+
 const glide = new Glide('.glide', {
   type: 'carousel',
 }).mount();
 
+// REZEPT VORHER EINKLAPPEN
 glide.on('run.before', function () {
   $('.recipe-info').slideUp();
 })
 
+// REZEPT ERWEITERN BUTTON
 $('.expand-recipe-info').click((e)=>{
 
   const buttonParent = e.target.parentNode;
